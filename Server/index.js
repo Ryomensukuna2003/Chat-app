a3c5322d8d2be3759f1e9bcf36af23789948ba9b
Shivanshu <mshivanshu1264@gmail.com>
2024-03-04 13:10:18 +0530

    added rooms in sockets in server folder

--- Server/index.js
+++ Server/index.js
@@ -1,33 +1,36 @@
-const express=require("express");
-const http=require("http");
-const bodyParser=require("body-parser");
-const {Server}=require("socket.io");
-require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
-const app=express();
-const server=http.createServer(app);
-const io=new Server(server);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-
-// const PORT=process.env.PORT;
-const PORT= 5000;
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/Views/index.html');
+app.use(express.static(path.join(__dirname, 'public')));
+const PORT = 5000;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-io.on('connection',(socket)=>{
-    console.log('User Connected',socket.id);
-    socket.on('disconnect',()=>{
+io.on('connection', (socket) => {
+    console.log('User Connected', socket.id);
+    socket.on("chat", (res, room) => {
+        if (room === '') {
+            io.emit("sending_to_client", res); // For all
+        }
+        else {
+            io.to(room).emit("sending_to_client", res);
+        }
+    })
+    socket.on("join", (room) => {
+        socket.join(room);
+    })
+    socket.on('disconnect', () => {
         console.log('User Disconnected');
     })
-    socket.on("chat message",(msg)=>{
-        console.log('message: '+msg);
-        io.emit('chat message',msg);
-    })
 })
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Server Listning at ${PORT}`);
 })
