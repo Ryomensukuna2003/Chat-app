083a2cad8dc3ee931cdf945e78005b50f5853011
Shivanshu <mshivanshu1264@gmail.com>
2024-03-06 18:34:27 +0530

    Added some tables

--- Server/Models/db_model.js
+++ Server/Models/db_model.js
@@ -11,10 +11,32 @@ const dbConfig = {
 
 // Create a new PostgreSQL client
 const client = new Client(dbConfig);
-client.connect()
-    .then(() => {
-        console.log('Connected to PostgreSQL database');
-    })
-    .catch((err) => {
-        console.error('Error connecting to PostgreSQL database', err);
-    });
+
+const execute = async (query) => {
+    try {
+        await client.connect();     // gets connection
+        await client.query(query);  // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    } finally {
+        await client.end();         // closes connection
+    }
+};
+
+
+const text = `
+    CREATE TABLE IF NOT EXISTS "users" (
+        user_id         SERIAL          PRIMARY KEY,
+        username        VARCHAR(50)     UNIQUE NOT NULL,
+        password_hash   VARCHAR(60)     NOT NULL, 
+        email           VARCHAR(100)    UNIQUE NOT NULL
+    );`;
+
+execute(text).then((result) => {
+    if (result) {
+        console.log('Table created');
+    }
+});
+
