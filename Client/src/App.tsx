a90487f96327c1aa08b3f8632184796490cf6d4e
Shivanshu <mshivanshu1264@gmail.com>
2024-03-13 22:49:01 +0530

    Added Client to Server and resolved CORS error

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,10 @@
 import Layout from "./Layout";
 import HomePage from "@/components/HomePage";
-// import { SiteHeader } from "./components/site-header";
 
 function App() {
   return (
     <Layout>
       <HomePage />
-      {/* <SiteHeader /> */}
     </Layout>
   );
 }
--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,28 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+import cors from 'cors';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+import bodyParser from 'body-parser';
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:5173', // Only allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+
 const PORT = 5000;
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.post('/api/login', (req, res) => {
+    const { name, password } = req.body;
+    console.log('Name:', name);
+    console.log('Password:', password);
+
+    res.status(200).json({ message: 'Login successful' });
 })
 
 io.on('connection', (socket) => {
